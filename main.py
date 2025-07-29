08095afd3b08d781c455822fd59b0c9eb3b13a0a
rexchoppers <connorbrady@codox.uk>
2025-07-29 08:57:21 +0100

    ADDED dotenv

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+# Load environment variables from a .env file
+load_dotenv()
 
 app = FastAPI()
 
 class Query(BaseModel):
     q: str
-
 
 @app.post("/q")
 async def query(query: Query):
